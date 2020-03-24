51faf7f06ee8541bd2e1d58cd47e05ea0f29861f
golbanv <golbanvladislav@gmail.com>
2020-03-24 15:30:07 +0100

    Add input fields

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducers from "./reducers/index";
+
+const composeEnhasers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhasers(applyMiddleware()));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
